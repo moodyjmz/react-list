1e4a79bd4f8aa8f35b921184d97dab4f28538cd2
James Manuel <moodyjmz@users.noreply.github.com>
2025-08-19 01:08:49 +0200

    feat: :white_check_mark: Added API test

---
+++ src/feature/popularRepos/api/__tests__/fetchPopularRepos.test.ts
@@ -0,0 +1,38 @@
+import { test, vi, expect } from 'vitest';
+import { fetchPopularRepos } from '../fetchPopularRepos';
+
+test('fetches data successfully from API', async () => {
+    const lastWeek = new Date();
+    lastWeek.setDate(lastWeek.getDate() - 7);
+    const dateStr = lastWeek.toISOString().split('T')[0];
+    // Mock the fetch function.
+    const mockResponse = {
+        items: [
+            {
+                'id': 1,
+                'name': 'Test Name',
+                'stargazers_count': 100,
+                'language': 'testScript',
+                'html_url': 'https://github.com/Test',
+                'description': 'Test Description'
+            }
+        ]
+    };
+
+    // Here we tell Vitest to mock fetch on the `window` object.
+    global.fetch = vi.fn(() =>
+        Promise.resolve({
+            ok: true,
+            json: () => Promise.resolve(mockResponse),
+        }),
+    );
+
+    // Call the function and assert the result
+    const data = await fetchPopularRepos();
+    expect(data.repos).toEqual(mockResponse.items);
+    expect(data.languages).toEqual(['testScript']);
+
+    // Check that fetch was called exactly once
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(`https://api.github.com/search/repositories?q=created:>${dateStr}&sort=stars&order=desc`);
+});
