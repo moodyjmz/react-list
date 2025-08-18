fb3b5c1565efd517641c7800208fa61512eec6a4
James Manuel <moodyjmz@users.noreply.github.com>
2025-08-19 00:30:17 +0200

    fix: :bug: Added type guard for item raw processing

--- src/feature/popularRepos/api/fetchPopularRepos.ts
+++ src/feature/popularRepos/api/fetchPopularRepos.ts
@@ -31,22 +31,22 @@ const createRepoItem = (item: ApiRepo, keys: string[]): Repo => {
 export async function fetchPopularRepos(): Promise<PopularReposResponse> {
   const lastWeek = new Date();
   lastWeek.setDate(lastWeek.getDate() - 7);
-  const dateStr = lastWeek.toISOString().split("T")[0];
+  const dateStr = lastWeek.toISOString().split('T')[0];
 
   const res = await fetch(
     `https://api.github.com/search/repositories?q=created:>${dateStr}&sort=stars&order=desc`
   );
   if (!res.ok) {
-    throw new Error("Failed to fetch popular repositories");
+    throw new Error('Failed to fetch popular repositories');
   }
   const raw = await res.json();
   // const raw = api;
   // post process data to extract only what we want
   const languagesSet: Set<string> = new Set();
   const repos: Repo[] = [];
-  raw?.items.forEach((item) => {
+  raw?.items.forEach((item: ApiRepo) => {
     // get languages and extract item data we care about
-    item.language && languagesSet.add(item.language);
+    item.language && typeof item.language === 'string' && languagesSet.add(item.language);
     repos.push(createRepoItem(item, extractKeys));
   });
   const languages: string[] = Array.from(languagesSet);
